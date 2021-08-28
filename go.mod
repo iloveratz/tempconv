f24b7a0e0c4891dc142218594a598b72e2b918f7
Shawn McMahon <shawn.mcmahon@starz.com>
2021-08-28 12:25:27 -0600

    intialize

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/iloveratz/tempconv
+
+go 1.17
---
+++ tempconv1.go
@@ -0,0 +1,10 @@
+package tempconv
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
